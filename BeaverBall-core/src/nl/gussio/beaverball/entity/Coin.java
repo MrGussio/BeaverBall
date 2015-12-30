770e46ea2a8ab8cd1b0e7005793a8c9d545664fd
MrGussio <jellehulter@hotmail.com>
2015-12-30 18:34:55 +0100

    Created Coin entity.

---
+++ BeaverBall-core/src/nl/gussio/beaverball/entity/Coin.java
@@ -0,0 +1,64 @@
+package nl.gussio.beaverball.entity;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+import nl.gussio.beaverball.CurrencyManager;
+import nl.gussio.beaverball.screen.GameScreen;
+import nl.gussio.beaverball.screen.ScreenManager;
+
+public class Coin extends Entity {
+
+	public Sprite texture;
+	public Rectangle collision;
+	public EntityManager em;
+	
+	public Coin(int x, int y, EntityManager em) {
+		super(x, y);
+		texture = new Sprite(new Texture(Gdx.files.internal("coin.png")));
+		this.em = em;
+	}
+
+	@Override
+	public void render(SpriteBatch sb) {
+		sb.draw(texture, location.x, location.y);
+	}
+
+	@Override
+	public void update() {
+		if(collidesWithPlayer()){
+			CurrencyManager.addCoin();
+			em.removeEntity(this);
+		}
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+	@Override
+	public void create() {
+		
+	}
+	
+	private boolean collidesWithPlayer() {
+		if(ScreenManager.getStaticScreen() instanceof GameScreen){
+			GameScreen gs = (GameScreen) ScreenManager.getStaticScreen();
+			for(Entity e : gs.em.entities){
+				if(e instanceof Player){
+					Player p = (Player) e;
+					if(p.collision.overlaps(collision)){
+						return true;
+					}
+				}
+			}
+		}
+		
+		return false;
+	}
+
+}
