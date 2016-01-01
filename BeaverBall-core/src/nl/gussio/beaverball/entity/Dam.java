912bfa7fcec2a3478cc0498966e15bf378255f44
MrGussio <jellehulter@hotmail.com>
2016-01-01 20:52:08 +0100

    Created the Dam entity.

---
+++ BeaverBall-core/src/nl/gussio/beaverball/entity/Dam.java
@@ -0,0 +1,34 @@
+package nl.gussio.beaverball.entity;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Dam extends Entity {
+
+	public int health;
+	
+	public Dam(int x, int y, int health) {
+		super(x, y);
+		this.health = health;
+	}
+
+	@Override
+	public void render(SpriteBatch sb) {
+		
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+	@Override
+	public void create() {
+		
+	}
+
+}
