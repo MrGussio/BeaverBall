be280665b9014ff1ba51c844f97237280ad6a585
MrGussio <jellehulter@hotmail.com>
2016-01-02 14:20:14 +0100

    Created MenuScreen

---
+++ BeaverBall-core/src/nl/gussio/beaverball/screen/MenuScreen.java
@@ -0,0 +1,39 @@
+package nl.gussio.beaverball.screen;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuScreen extends Screen{
+
+	@Override
+	public void render(SpriteBatch sb) {
+		
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+	@Override
+	public void create() {
+		
+	}
+
+	@Override
+	public void resize(int width, int height, float aspectRatio) {
+		
+	}
+
+	@Override
+	public void handleKey(float x, float y) {
+		
+	}
+
+	
+	
+}
