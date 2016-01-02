661d176ce5ecb1412443fee3907a4594a80aca44
MrGussio <jellehulter@hotmail.com>
2016-01-02 14:23:47 +0100

    Added textures and those are being rendered currently.ö

--- BeaverBall-core/src/nl/gussio/beaverball/entity/Dam.java
+++ BeaverBall-core/src/nl/gussio/beaverball/entity/Dam.java
@@ -1,19 +1,24 @@
 package nl.gussio.beaverball.entity;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Dam extends Entity {
 
+	public Sprite texture;
 	public int health;
 	
-	public Dam(int x, int y, int health) {
-		super(x, y);
+	public Dam(int y, int health) {
+		super((int) Math.random()*(Gdx.graphics.getWidth()-50), y);
 		this.health = health;
+		this.texture = new Sprite(new Texture(Gdx.files.internal("dam.png")));
 	}
 
 	@Override
 	public void render(SpriteBatch sb) {
-		
+		sb.draw(texture, location.x, location.y);
 	}
 
 	@Override
